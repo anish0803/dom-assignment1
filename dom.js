b73c06a6aef5b19b3a23cea1f6b0f49cb369a8d5
anish0803 <singhanish124@gmail.com>
2022-07-30 21:04:02 +0530

    dom assignment

---
+++ dom.js
@@ -0,0 +1,23 @@
+document.querySelector(".class1").innerText = "Anish Ranjan";
+document.querySelectorAll("div")[0].style.fontWeight = "bold";
+document.querySelector(".class2").innerText =
+  "Hello Everyone i am Anish Ranjan from Aurangabad Bihar, i am recently pursuing from bootcamp named Aircampus in  web development ";
+document.querySelector(".class3").style.backgroundColor = "red";
+document.querySelectorAll(".divs")[0].style.border = "black solid";
+document.querySelectorAll(".divs")[0].innerText = "Fourth Div";
+document.querySelectorAll(".divs")[1].style.border = "black solid";
+document.querySelectorAll(".divs")[1].innerText = "Fifth Div";
+document.querySelectorAll(".divs")[2].style.border = "black solid";
+document.querySelectorAll(".divs")[2].innerText = "Sixth Div";
+let createElement = document.createElement("span");
+let textnod = document.createTextNode("Aircampus");
+createElement.appendChild(textnod);
+document.getElementById("id1").appendChild(createElement);
+document.querySelectorAll("div")[8].innerHTML =
+  "<div class='new_htlm'><p>This is my first js assignment in DOM</p> </div> ";
+document.querySelectorAll("div")[10].innerText = "My Experience with Aircampus";
+document.querySelectorAll("div")[10].style.textDecoration = "underline";
+document.querySelectorAll("div")[10].style.fontWeight = "bold";
+document.querySelectorAll("div")[10].style.fontSize = "25px";
+document.querySelectorAll("div")[10].style.color = "blue";
+document.querySelectorAll("p")[1].innerText =" First of All I would like to say thank you AirCampus .All experiences in aircampus is very good,mentor's are really well to teach us ";
---
+++ readme.md
@@ -0,0 +1,4 @@
+<p>this is output of dom assignment1</p>
+
+
+<img src="image/output.PNG">
